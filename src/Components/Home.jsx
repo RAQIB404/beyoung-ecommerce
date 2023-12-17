016820077fc66633739714aa47dd08f77a539603
RAQIB404 <raqibkhan2332@gmail.com>
2023-12-17 17:03:22 +0530

    beyoung

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,7 @@ import  bb from "../Components/Assets/bb.png";
 import  hero1 from "../Components/Assets/hero1.png";
 import  homem from "../Components/Assets/homem.png";
 import ProductList from "./ProductList";
+import { NavLink } from "react-router-dom";
 import "./home.css"
 
 function Home() {
@@ -11,14 +12,18 @@ function Home() {
         <div className="deskhome">
             <div  className="silder">
                 <div className="heroImg">
+                <NavLink to="/productlist">
                     <img src={bb} alt="Hero 1" />
+                    </NavLink>
                 </div>
                 <div className="hero">
                     <img src={hero1} alt="Hero 2" />
                 </div>
             </div>
             <div className="homemob">
+            <NavLink to="/productlist">
                 <img src={homem} alt=" "/>
+                </NavLink>
             </div>
             <ProductList />
         </div>
